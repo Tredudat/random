58dffa78dfb63a5029dd83f51f87ea3cb1ba8a18
Trenton Henderson <109951199+Tredudat@users.noreply.github.com>
2024-02-27 00:04:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const homeButton = document.querySelector("nav a[href='#']");
-    const updatesButton = document.querySelector("nav a[href='updates']");
-  
-    homeButton.addEventListener("click", function(event) {
-      event.preventDefault();
-      window.location.href = "https://tredudat.github.io/my-website/";
-    });
-  
-    updatesButton.addEventListener("click", function(event) {
-      event.preventDefault();
-      window.location.href = "https://tredudat.github.io/my-website/updates.html";
-    });
+  const homeButton = document.querySelector(".navbar-link[href='#']");
+  const updatesButton = document.querySelector(".navbar-link[href='updates.html']");
+
+  homeButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    window.location.href = "https://tredudat.github.io/my-website/";
   });
+
+  updatesButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    window.location.href = "https://tredudat.github.io/my-website/updates.html";
+  });
+});
