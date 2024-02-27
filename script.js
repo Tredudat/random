78a3ad358e6fbe8a99034773f6fd6160509a0ad9
Trenton Henderson <109951199+Tredudat@users.noreply.github.com>
2024-02-27 01:07:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const homeButton = document.querySelector(".navbar-link[href='#']");
-  const updatesButton = document.querySelector(".navbar-link[href='updates.html']");
+ // Animation for profile picture
+ gsap.to(".profile-image", { duration: 2, x: 100, opacity: 1, rotation: 360 });
 
-  homeButton.addEventListener("click", function(event) {
-    event.preventDefault();
-    window.location.href = "https://tredudat.github.io/my-website/";
-  });
+ // Animation for big picture
+ gsap.to(".big-picture img", { duration: 2, y: -100, opacity: 1, scale: 1 });
 
-  updatesButton.addEventListener("click", function(event) {
-    event.preventDefault();
-    window.location.href = "https://tredudat.github.io/my-website/updates.html";
-  });
+ const homeButton = document.querySelector(".navbar-link[href='#']");
+ const updatesButton = document.querySelector(".navbar-link[href='updates.html']");
+
+ homeButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  window.location.href = "https://tredudat.github.io/my-website/";
+ });
+
+ updatesButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  window.location.href = "https://tredudat.github.io/my-website/updates.html";
+ });
 });
